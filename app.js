5d7d13a05fba10a70061ec3d7912c3d52fc03375
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2022-12-08 17:10:34 -0800

    added delete button appended at every input submission, not functional yet

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const amount = document.getElementById('itemAmount');
 const submitBtn = document.getElementById('submit');
 
 const table = document.getElementById('table');
+table.style.borderCollapse = 'collapse';
+
 const tbody = document.getElementById('tbody');
+
+const delBtn = document.createElement('button');
+delBtn.innerHTML = 'X';
 
 
 submitBtn.addEventListener('click', () => {
@@ -17,25 +22,34 @@ submitBtn.addEventListener('click', () => {
         }
      ]
 
-     let newRow = table.insertRow();
+     let newRow = tbody.insertRow();
      let cell1 = newRow.insertCell()
      let cell2 = newRow.insertCell()
      let cell3 = newRow.insertCell();
+     let cell4Del = newRow.insertCell()
 
      arrOfData.forEach(input => {
+        
         cell1.innerHTML = input.name;
         cell2.innerHTML = input.date;
         cell3.innerHTML = '$' + input.amount;
+        cell4Del.append(delBtn);
+
      })
 
     
     clearInputs();
 })
 
+//function to clear input field AFTER clicking submit button
 function clearInputs() {
     input.value = ''
     date.value = ''
     amount.value = '';
 }
 
+//funcition to delete table row 
+// function deleteButton() {
+    
+// }
 
