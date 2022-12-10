1b7235730b0ded775193c3225424e5c92052ccb7
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2022-12-09 18:50:08 -0800

    fix bug where delete button was not appeding to every row, confirmation func to delete

--- app.js
+++ app.js
@@ -1,57 +1,70 @@
-const input = document.getElementById('itemExpense');
+const nameOfExpense = document.getElementById('itemExpense');
 const date = document.getElementById('itemDate');
 const amount = document.getElementById('itemAmount');
 const submitBtn = document.getElementById('submit');
 const tbody = document.getElementById('tbody');
 const table = document.getElementById('table');
 
+
 table.style.borderCollapse = 'collapse';
-
-
-
-const delBtn = document.createElement('button');
-delBtn.onclick = deleteRow;
-delBtn.innerHTML = 'X';
 
 
 //function to clear input field AFTER clicking submit button
 function clearInputs() {
-    input.value = ''
+    nameOfExpense.value = ''
     date.value = ''
     amount.value = '';
 }
 
 
-function deleteRow() {
+function deleteRowData() {
+    let isTrue = true;
+    if (isTrue) {
+        confirm('Are you sure you want to delete this row?')
         tbody.deleteRow(-1);
-
+    } else {
+        isTrue = false;
+    
+    }
  }
  
+ function editRowData() {
 
-submitBtn.addEventListener('click', () => {
-    
+ }
+
+ 
+
+function inputToTableRow() {
     const arrOfData = [
         { 
-            name: input.value, 
+            name: nameOfExpense.value, 
             date: date.value, 
             amount: amount.value
         }
      ]
 
-    let newRow = tbody.insertRow();
+    let newRow = tbody.insertRow(tbody.length);
     let cell1 = newRow.insertCell(0);
     let cell2 = newRow.insertCell(1);
     let cell3 = newRow.insertCell(2);
     let cell4Del = newRow.insertCell(3);
 
-     arrOfData.forEach(input => {
-        
+    arrOfData.forEach(input => {
         cell1.innerHTML = input.name;
         cell2.innerHTML = input.date;
         cell3.innerHTML = '$' + input.amount;
-        cell4Del.append(delBtn);
-     })
+        cell4Del.innerHTML = `
+            <button onclick="deleteRowData()">
+                <i class="fa-solid fa-trash"></i>
+            </button>
+            <button onclick="editRowData()">
+                    Edit
+            </button>
+        `
+     });
 
     clearInputs();
 
-})
+}
+
+submitBtn.addEventListener('click', inputToTableRow);
