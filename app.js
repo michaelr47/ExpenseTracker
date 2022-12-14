3db5dde9a4a77dffbf8fcff4edc09d1c8271e9f6
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2022-12-13 21:06:47 -0800

    added update button in editRow function, little styling and html elements

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const nameOfExpense = document.getElementById('itemExpense');
-const date = document.getElementById('itemDate');
-const amount = document.getElementById('itemAmount');
-const submitBtn = document.getElementById('submit');
-const tbody = document.getElementById('tbody');
-const table = document.getElementById('table');
-let selectedRow = null;
+// ----------------------------------------------------------------------------------------------|
+const nameOfExpense = document.getElementById('itemExpense'); // --------------------------------|
+const date          = document.getElementById('itemDate'); // -------------------------------|   |
+const amount        = document.getElementById('itemAmount'); // -------------------------|   |   |
+//  ---------------------------------------------------------- ACCESSING HTML ELEMENTS ----------|
+const submitBtn     = document.getElementById('submit'); // -----------------------------|   |   |
+const table         = document.getElementById('table'); // ----------------------------------|   |  
+const tbody         = document.getElementById('tbody'); // --------------------------------------|
+// ----------------------------------------------------------------------------------------------|
 
-table.style.borderCollapse = 'collapse';
-
+// declaring variables in global scope to assingm new values in functions later..
+let selectedRow = null;      
+let fourthCell;
 
 //function to clear input field AFTER clicking submit button
 function clearInputs() {
@@ -16,95 +19,107 @@ function clearInputs() {
     amount.value = '';
 }
 
-
-function deleteRowData(td) {
-
-    let isTrue = true;
-    if (confirm('Are you sure you want to delete this row?') !== isTrue ) {
-        isTrue = false;
-    } else {
-        let row = td.parentNode.parentNode;
+//function executes upon clicking trash button to delete a specific row
+function deleteRowData(el) {
+    let userConfirmed = confirm('Are you sure you want to delete this row?');
+    if (userConfirmed) {
+        let row = el.parentNode.parentNode;
         row.parentNode.removeChild(row);
     }
- }
+}
 
- let dataTypes = [
+// array to iterate through assigning input types to inputs
+ let inputTypes = [
     'text',
     'date',
-    'text'
+    'number'
 ];
 
-let inputTypes = [
+// array of id's to assign the input elements
+let inputIDs = [                           
     'inputName',
     'inputDate',
     'inputAmount'
 ];
- 
- function editRowData(data) {
-    
-    let selectedRow = data.parentElement.parentElement;
-	
-	for (let i = 0; i < selectedRow.cells.length - 1; i++) {
-		let cell = selectedRow.cells[i]
-		let input = document.createElement('input');
 
-        cell.innerText = '';
-		input.setAttribute('type', dataTypes[i]);
-		input.setAttribute('id', inputTypes[i]);
+
+//function to to edit all 3 cells in corresponding row upon clicking edit button
+ function editRowData(data) {
+
+    let selectedRow = data.parentElement.parentElement;  // gets the row from table
+    
+    fourthCell = selectedRow.cells.length - 1;     // assigning variable to the last cell
+
+    // looping through cells in a row to create inputs, assigning input types and id's and then appending them to each cell
+	for (let i = 0; i < selectedRow.cells.length - 1; i++) { 
+		let cell = selectedRow.cells[i];
+		let input = document.createElement('input');
+        input.value = cell.innerText;
+
+        cell.innerText = ''; // clears the text content when clicking edit
+		input.setAttribute('type', inputTypes[i]); 
+		input.setAttribute('id', inputIDs[i]); 
 		cell.append(input);
 	}
 
-    
+    // creating update button...       
     const updateBtn = document.createElement('button');
-    updateBtn.innerText = 'Update';
-
-    selectedRow.cells[3].append(updateBtn);
-
-
+    updateBtn.innerText = 'Update'; 
+    // ... and appending it into last cell         
+    selectedRow.cells[fourthCell].append(updateBtn);
+    // event listener on updateBtn to get new values of each cell update them to their corresponding cell
     updateBtn.addEventListener('click', () => {
+        // loop through cells minus the last one 
         for (let i = 0; i < selectedRow.cells.length - 1; i++) {
-			
-            let cell = selectedRow.cells[i];
-			let type = cell.getElementsByTagName('input')[0].type;
-
-			if (type == "text") {
-                cell.innerText = document.getElementById(inputTypes[i]).value;
-			} else {
-                cell.textContent = document.getElementById(inputTypes[i]).value;
+            
+            let cell = selectedRow.cells[i];  // asssing 'cell' to each cells there are ... 
+			let type = cell.getElementsByTagName('input')[0].type; // ... getting their input types...
+             
+			if (type == 'text') {          // if type = text, get innerText values appended
+                cell.innerText = document.getElementById(inputIDs[i]).value;
+			} else if (type == 'number') {             // if type = number, get the innerText aswell AND appending '$' behind the amount
+                cell.innerText = '$' + document.getElementById(inputIDs[i]).value;
+            } else {      // else, which is the type = date, get the textContent appended
+                cell.textContent = document.getElementById(inputIDs[i]).value;
             }
         }
-
-        updateBtn.style.display = 'none';
+        //end of loop
+        
+        updateBtn.style.display = 'none'; // upon clicking button, it disappears...
     });
 
 }
 
 
-
+// funciion to get input values appended to each cell
 function inputToTableRow() {
+    let trNoExpenseText = document.getElementById('noDataYet'); // accessing tr html element and ........
+    trNoExpenseText.style.display = 'none'; // ....... displaying it to 'none' upon clicking submit
 
-    const arrOfData = [
+    if (!nameOfExpense.value || !date.value || !amount.value) { return } // checking for validation on each input field
+        // an array of objects to iterate through ..
+    const arrOfData = [     
         { 
             name: nameOfExpense.value, 
             date: date.value, 
             amount: amount.value
         }
      ]
-
+    // creating new rows and cells
     let newRow = tbody.insertRow(tbody.length);
     let cell1 = newRow.insertCell(0);
     let cell2 = newRow.insertCell(1);
     let cell3 = newRow.insertCell(2);
     let cell4Del = newRow.insertCell(3);
-    
 
+     // utilizing forEach method to grab each input values and append it to cells
     arrOfData.forEach(input => {
         cell1.textContent = input.name;
         cell2.textContent = input.date;
         cell3.textContent = '$' + input.amount;
         cell4Del.innerHTML = 
         `
-            <button onclick="deleteRowData(this)" style="padding: 1px;">
+            <button onclick="deleteRowData(this)">
                 <i class="fa-solid fa-trash"></i>
             </button>
             <button onclick="editRowData(this)">
@@ -112,25 +127,11 @@ function inputToTableRow() {
             </button>
         `
      });
-
+     // calling this func to clear input after clicking submit btn.
     clearInputs();
-
-}
-
-function checkValidityOnInputs() {
-    // let reg = new RegExp('^[0-9]*$')
-    if (!nameOfExpense.value && !date.checkValidity() && !amount.value) {
-        alert('Please fill in all 3 fields.')
-        submitBtn.disabled = true;
-    } 
-}
-
-submitBtn.addEventListener('click', () => {
     
-    checkValidityOnInputs();
-    inputToTableRow();
-
-});
-
+}
+// and of course adding an event listener to submit button 😅
+submitBtn.addEventListener('click', inputToTableRow);
 
 
